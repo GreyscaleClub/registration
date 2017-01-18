2c6cf4a0174a615302ac9cca7662da646d7a88c0
Carl Masri <carl.masri@gmail.com>
2017-01-17 18:32:19 -0800

    Add RegistrationForm component

---
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title">CTF Registration Form</h1>
+        <h2 className="description">Please fill in the following information to register for the CTF event</h2>
+      </div>
+    );
+  }
+}
+
+export default RegistrationForm;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import BurgerMenu from './components/BurgerMenu/BurgerMenu.js';
+import RegistrationForm from './components/RegistrationForm/RegistrationForm.js'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div id="outer-container" style={{ height: '100%' }}>
         <BurgerMenu />
+        <main>
+          <RegistrationForm />
+        </main>
       </div>
     );
   }
